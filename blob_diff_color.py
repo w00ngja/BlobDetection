3e066581b5c79c5802be802f9764022f59a230ac
Jaewoong Hwang <noa9925@gmail.com>
2023-03-30 18:31:50 +0900

    2nd commit
    add Shape difference Algorithm

--- blob_diff_color.py
+++ blob_diff_color.py
@@ -25,7 +25,7 @@ def video_play():
     if ret:
         start_t = timeit.default_timer()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         # 빨간 점 필터 마스크 (keypoints 추출에 사용)
         frame_r = cv2.inRange(frame, (170, 50, 50), (180, 255, 255))
@@ -33,7 +33,7 @@ def video_play():
         frame_r = cv2.dilate(frame_r, None, iterations=0)
 
         # 초록 점 필터 마스크 (keypoints 추출에 사용)
-        frame_g = cv2.inRange(frame, (25, 57, 2), (102, 255, 255))
+        frame_g = cv2.inRange(frame, (110,100,100), (130, 255, 255))
         frame_g = cv2.erode(frame_g, None, iterations=0)
         frame_g = cv2.dilate(frame_g, None, iterations=0)
 
@@ -44,9 +44,9 @@ def video_play():
         dst2 = cv2.bitwise_and(frame,frame,mask=frame_g)
 
         # 합쳐서 frame으로 넘겼음 -> (초록 + 빨간 점 확인 위한 화면 출력용으로, 실제 키포인트 인식은 개별 마스크를 통해 진행)
-        np.add(dst1,dst2,frame)
-        frame = cv2.erode(frame, None, iterations=0)
-        frame = cv2.dilate(frame, None, iterations=0)
+        # np.add(dst1,dst2,frame)
+        # frame = cv2.erode(frame, None, iterations=0)
+        # frame = cv2.dilate(frame, None, iterations=0)
 
         h, w = gray.shape[:2]
         keypoints = detector.detect(gray)
@@ -67,41 +67,36 @@ def video_play():
             lazer_r_x.set("lazer_x (red) : " + str(int(lz_x_r)))
             lazer_r_y.set("lazer_y (red) : " + str(int(lz_y_r)))
 
-        # im_with_keypoints_g = cv2.drawKeypoints(frame_g, keypoints_g, np.array([]), (0, 2500, 0),
-        #                                       cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        # im_with_keypoints_r = cv2.drawKeypoints(frame_r, keypoints_r, np.array([]), (0, 2500, 0),
-        #                                         cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
 
         im_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0, 2500, 0),
                                                 cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-        edges = cv2.Canny(gray, 100, 200)
-        corners = cv2.goodFeaturesToTrack(gray, 5, 0.04, 10)
-
-        corners = np.int0(corners).reshape(corners.shape[0], corners.shape[2])
-
-        xy_12 = corners[corners[:, 0].argsort(kind='mergesort')]
-        xy_34 = corners[corners[:, 1].argsort(kind='mergesort')]
-        x1, y1 = xy_12[0];
-        x2, y2 = xy_12[-1];
-        x3, y3 = xy_34[0];
-        x4, y4 = xy_34[-1]
-
-        px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / \
-             ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
-
-        py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / \
-             ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
-
-        if np.isnan(px) == False and np.isnan(py) == False:
-            ##            print(int(px), int(py))
-            cv2.circle(im_with_keypoints, (int(px), int(py)), 10, (0, 0, 255), 3)
-            cross_x.set("cross_x : " + str(int(px)))
-            cross_y.set("cross_y : " + str(int(py)))
-        for i in corners:
-            x, y = i.ravel()
-            cv2.circle(im_with_keypoints, (x, y), 3, 255, -1)
+        # edges = cv2.Canny(gray, 100, 200)
+        # corners = cv2.goodFeaturesToTrack(gray, 5, 0.04, 10)
+        #
+        # corners = np.int0(corners).reshape(corners.shape[0], corners.shape[2])
+        #
+        # xy_12 = corners[corners[:, 0].argsort(kind='mergesort')]
+        # xy_34 = corners[corners[:, 1].argsort(kind='mergesort')]
+        # x1, y1 = xy_12[0];
+        # x2, y2 = xy_12[-1];
+        # x3, y3 = xy_34[0];
+        # x4, y4 = xy_34[-1]
+        #
+        # px = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / \
+        #      ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
+        #
+        # py = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / \
+        #      ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
+        #
+        # if np.isnan(px) == False and np.isnan(py) == False:
+        #     ##            print(int(px), int(py))
+        #     cv2.circle(im_with_keypoints, (int(px), int(py)), 10, (0, 0, 255), 3)
+        #     cross_x.set("cross_x : " + str(int(px)))
+        #     cross_y.set("cross_y : " + str(int(py)))
+        # for i in corners:
+        #     x, y = i.ravel()
+        #     cv2.circle(im_with_keypoints, (x, y), 3, 255, -1)
 
         terminate_t = timeit.default_timer()
         fps = int(1. / (terminate_t - start_t))
@@ -111,7 +106,7 @@ def video_play():
         # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # 출력 화면 지정
-        img = Image.fromarray(frame)
+        img = Image.fromarray(im_with_keypoints)
         imgtk = ImageTk.PhotoImage(image=img)
         label2.imgtk = imgtk
         label2.configure(image=imgtk)
